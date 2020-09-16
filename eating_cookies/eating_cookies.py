a48f8c021cd20df3b4183e97638afea74a54aa07
psaul <sean.backstrom@gmail.com>
2020-09-16 16:13:57 -0700

    frst

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,11 +4,24 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+    cap = 3
+    o = n - 2
+    p = 1
+    if n == 0:
+        return 1
+    if n < 3:
+        return n
+    if n >= 3:
+        return n * o + p
+    
+    
+    
 
-    pass
+    return 
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 6
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+
